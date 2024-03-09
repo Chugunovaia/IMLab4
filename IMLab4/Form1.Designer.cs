400b17fbb53f72755a64e736ab8de1cf738b7acc
Chugunovaia <145828554+Chugunovaia@users.noreply.github.com>
2024-03-09 21:51:27 +0700

    Create Form1.Designer.cs

---
+++ IMLab4/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace IMLab4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.nCells = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.nCells)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(600, 600);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(635, 112);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Start/Stop";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// nCells
+			// 
+			this.nCells.Location = new System.Drawing.Point(635, 36);
+			this.nCells.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+			this.nCells.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			this.nCells.Name = "nCells";
+			this.nCells.Size = new System.Drawing.Size(120, 22);
+			this.nCells.TabIndex = 2;
+			this.nCells.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(632, 17);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(145, 16);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Закрашенные клетки";
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(898, 627);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.nCells);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.nCells)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown nCells;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
